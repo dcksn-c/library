a253eeeb62c899d09c4eddf820efbb3d05ebf090
Dickson <116993989+dcksn-c@users.noreply.github.com>
2023-02-23 03:10:11 +0800

    first version of library project

---
+++ script.js
@@ -0,0 +1,74 @@
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+Book.prototype.readToggle = function() {
+    this.read = !this.read;
+}
+
+function toggleRead(index) {
+    myLibrary[index].readToggle();
+    addBookCard();
+}
+
+function addBookToLibrary() {
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let read = document.querySelector("#read").checked;
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  addBookCard();
+}
+
+let newBookBtn = document.querySelector(".new-book-btn");
+newBookBtn.addEventListener("click", function() {
+    let bookForm = document.querySelector("#book-form");
+    bookForm.classList.remove("hide");
+})
+
+document.querySelector("#book-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    addBookToLibrary();
+});
+
+function addBookCard() {
+    let bookForm = document.querySelector("#book-form");
+    let library = document.querySelector(".library");
+    library.textContent = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+        let bookCard = document.createElement("div");
+        let book = myLibrary[i];
+        bookCard.classList.add("book-card");
+        bookCard.innerHTML = `
+        <h2>${book.title}</h2>
+        <p>${book.author}</p>
+        <p>${book.pages} pages</p>
+        <div class="read-status">${book.read ? "Read" : "Not Read Yet"}</div>
+        <button class="toggle-btn" onclick="toggleRead(${i})">Read Status</button>
+        <button class="remove-btn" onclick="removeBook(${i})">Remove Book</button>
+        `;
+        library.appendChild(bookCard);
+        // or change to <div class="read-status ${book.read ? "read" : "unread"}">
+        let readStatus = bookCard.querySelector(".read-status");
+        if (book.read) {
+            readStatus.classList.add("read");
+        }
+        else {
+            readStatus.classList.add("unread")
+        }
+
+    }
+
+    bookForm.classList.add("hide");    
+}
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    addBookCard();
+}
